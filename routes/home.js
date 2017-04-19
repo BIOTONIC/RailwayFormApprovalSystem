ef34c488e869245db8bc65a0ad3a1619915e298d
lovejoy <805197013@qq.com>
2017-04-19 16:37:56 +0800

    Query Complete

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var isLogin = require('../others/auth').isLogin;
+var getFormatTime = require('../others/util').getFormatTime;
 var getState = require('../others/util').getState;
 var app1Service = require('../services/app1Service');
 var app2Service = require('../services/app2Service');
@@ -34,15 +35,16 @@ router.get('/apply', isLogin, function (req, res, next) {
 router.get('/list', isLogin, function (req, res, next) {
     var person = req.session.userId.slice(0, 1);
     var need = ['id', 'workshop', 'nextperson'];
+    var query = {};
     var appResults = [];
 
     if (person == '1' || person == '2') {
         // list forms only from this workshop
         var workshop = req.session.userId.slice(1);
-        var query = {workshop: workshop};
+        query = {workshop: workshop};
     } else if (person == '3' || person == '4' || person == '5') {
         // list all forms
-        var query = {};
+        query = {};
     } else {
         // person wrong
         req.flash('error', '账号角色错误');
@@ -107,21 +109,50 @@ router.get('/query', isLogin, function (req, res, next) {
     res.render('query');
 });
 
-router.get('/queryByWorkshop',isLogin,function(req,res,next){
+router.get('/queryResult', isLogin, function (req, res, next) {
+    var person = req.session.userId.slice(0, 1);
+    var workshop = req.query.workshop;
+    var sqstarttime = req.query.sqstarttime;
+    var sqendtime = req.query.sqendtime;
+    var section = req.query.section;
 
-});
-
-router.get('/queryall', isLogin, function (req, res, next) {
     var need = ['id', 'workshop', 'nextperson'];
-    var order = [['applytime', 'ASC']];
+    var query = {};
     var appResults = [];
 
-    app1Service.find2(need, order).then((app1Results) => {
+    if (typeof workshop != 'undefined') {
+        query = {workshop: workshop};
+    } else if (typeof sqstarttime != 'undefined' && typeof sqendtime != 'undefined') {
+        if (sqstarttime != '' && sqendtime != '') {
+            sqstarttime = getFormatTime(sqstarttime);
+            sqendtime = getFormatTime(sqendtime);
+            query = {sqstarttime: {$gte: sqstarttime}, sqendtime: {$lte: sqendtime}};
+        } else if (sqstarttime != '' && sqendtime == '') {
+            sqstarttime = getFormatTime(sqstarttime);
+            query = {sqstarttime: {$gte: sqstarttime}};
+        } else if (sqstarttime == '' && sqendtime != '') {
+            sqendtime = getFormatTime(sqendtime);
+            query = {sqendtime: {$lte: sqendtime}};
+        } else {
+            res.redirect('back');
+        }
+    } else if (typeof section != 'undefined') {
+        if (section == '') {
+            res.redirect('back');
+        } else {
+            query = {section: section};
+        }
+    } else {
+        res.redirect('back');
+    }
+
+    app1Service.find(need, query).then((app1Results) => {
         if (app1Results != null && app1Results.length > 0) {
             for (var i = 0; i < app1Results.length; i++) {
                 var result = {};
-                result.level = '1';
                 result.id = app1Results[i].id;
+                result.href = '/app1?formId=' + result.id;
+                result.name = '一级审批表' + result.id;
                 result.workshop = app1Results[i].workshop;
                 result.nextperson = app1Results[i].nextperson;
                 appResults.push(result);
@@ -129,12 +160,13 @@ router.get('/queryall', isLogin, function (req, res, next) {
         }
     });
 
-    app2Service.find2(need, order).then((app2Results) => {
+    app2Service.find(need, query).then((app2Results) => {
         if (app2Results != null && app2Results.length > 0) {
             for (var i = 0; i < app2Results.length; i++) {
                 var result = {};
-                result.level = '2';
                 result.id = app2Results[i].id;
+                result.href = '/app2?formId=' + result.id;
+                result.name = '二级审批表' + result.id;
                 result.workshop = app2Results[i].workshop;
                 result.nextperson = app2Results[i].nextperson;
                 appResults.push(result);
@@ -142,24 +174,29 @@ router.get('/queryall', isLogin, function (req, res, next) {
         }
     });
 
-    app3Service.find2(need, order).then((app3Results) => {
+    app3Service.find(need, query).then((app3Results) => {
         if (app3Results != null && app3Results.length > 0) {
             for (var i = 0; i < app3Results.length; i++) {
                 var result = {};
-                result.level = '3';
                 result.id = app3Results[i].id;
+                result.href = '/app3?formId=' + result.id;
+                result.name = '三级审批表' + result.id;
                 result.workshop = app3Results[i].workshop;
                 result.nextperson = app3Results[i].nextperson;
                 appResults.push(result);
             }
         }
-
-        // TODO
-        // It's not good to push res.json() here if aap1Service and app2Service too long time than app3Service
-        // But I am a freshman to Javascript, so sorry.
-        res.json(appResults)
     });
 
+    setTimeout(function () {
+        for (var i = 0; i < appResults.length; i++) {
+            appResults[i].state = getState(person, appResults[i].nextperson);
+        }
+
+        res.locals.appResults = appResults;
+
+        res.render('list');
+    }, 1000);
 });
 
 module.exports = router;
